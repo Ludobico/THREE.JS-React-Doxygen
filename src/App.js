ddef6de5e2aea9b77334a2bcf7483b35d9e1ab8a
chanjin kim <aqs450@gmail.com>
2022-12-19 21:08:30 +0900

    three fiber

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import FirstMain from "./Components/FirstMain";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        {/** 처음 메인 화면 */}
+        <Route path="/" element={<FirstMain />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/FirstMain.jsx
+++ src/Components/FirstMain.jsx
@@ -1,9 +1,54 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import "./FirstMain.css";
 
 const FirstMain = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="content">
+        <header>
+          <div className="nav">
+            <ul className="nav_list">
+              <li>
+                <a href="#">
+                  <span>Home</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span>About me</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span>3D Background</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span>Projects</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span>Contact</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </header>
+        <div className="description">
+          <h1>THREE.JS</h1>
+          <h2>
+            This is <br />
+            3D Background <br />
+            Project
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FirstMain;
