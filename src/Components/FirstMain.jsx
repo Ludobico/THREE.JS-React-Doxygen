226b59c9def263278a281d9706a296ba35f95877
chanjin kim <aqs450@gmail.com>
2022-12-19 20:50:34 +0900

    main setting

---
+++ src/Components/FirstMain.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+const FirstMain = () => {
+  return <div></div>;
+};
+
+export default FirstMain;
