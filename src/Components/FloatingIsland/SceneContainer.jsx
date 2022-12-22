9a7371f138217ff31ee0e1424eed27af2872adc1
chanjin kim <aqs450@gmail.com>
2022-12-22 14:26:05 +0900

    JSDOC(Doxygen)

--- src/Components/FloatingIsland/SceneContainer.jsx
+++ src/Components/FloatingIsland/SceneContainer.jsx
@@ -10,6 +10,13 @@ import Rocks from "./Rocks";
 import Portal from "./Portal";
 import FloatingRocks from "./FloatingRocks";
 
+/**
+ * @author Ludobico <aqs450@gmail.com>
+ * @copyright Floating portals with React Three Fiber and Three.js
+ * @description 컴포넌트에서 불러온 모델들을 Scene으로 불러와주는 컴포넌트
+ * @error Enviroment 컴포넌트의 텍스쳐모델들은 모드 public에 들어있어야함, 또한 HDR확장자가 아닌경우 에러메세지 출력
+ */
+
 const SceneContainer = () => {
   return (
     <div className="SC_Canvas">
