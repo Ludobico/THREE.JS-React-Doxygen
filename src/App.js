fc64a5a1c636c8feb0237cd851bf623b82b5e392
chanjin kim <aqs450@gmail.com>
2023-01-22 01:17:56 +0900

    three postprocessig

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import FirstMain from "./Components/FirstMain";
 import SceneContainer from "./Components/FloatingIsland/SceneContainer";
 import { Canvas } from "@react-three/fiber";
-import React_three_fiber_tuto from "./Components/React_three_fiber_tuto";
-import ShaderMain from "./Components/orbit_shader/ShaderMain";
+import PostprocessingMain from "./Components/postprocessing/PostprocessingMain";
 
 function App() {
   return (
@@ -14,10 +13,8 @@ function App() {
         <Route path="/" element={<FirstMain />} />
         {/** FloatingIsland */}
         <Route path="/flot" element={<SceneContainer />} />
-        {/** fiber튜토리얼 */}
-        <Route path="/tuto" element={<React_three_fiber_tuto />} />
-        {/** 스크롤 및 쉐이더 */}
-        <Route path="/shader" element={<ShaderMain />} />
+        {/** 포스트 프로세싱 */}
+        <Route path="/post" element={<PostprocessingMain />} />
       </Routes>
     </div>
   );
