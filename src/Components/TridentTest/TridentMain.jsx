177c9474d7885887561d903e5b3e9775c9c06201
Ludobico <aqs450@gmail.com>
2023-04-14 09:17:51 +0900

    trident

---
+++ src/Components/TridentTest/TridentMain.jsx
@@ -0,0 +1,13 @@
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import Trident from "./Trident";
+
+const TridentMain = () => {
+  return (
+    <Canvas>
+      <Trident />
+    </Canvas>
+  );
+};
+
+export default TridentMain;
