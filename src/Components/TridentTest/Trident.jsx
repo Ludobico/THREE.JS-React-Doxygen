2b4f7cfdf532f1fcc11e9ef9cd9598891f872c67
chanjin kim <aqs450@gmail.com>
2023-04-22 22:23:16 +0900

    shadow gltf

--- src/Components/TridentTest/Trident.jsx
+++ src/Components/TridentTest/Trident.jsx
@@ -1,7 +1,7 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useHelper } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
-import React, { Suspense, useEffect, useState } from "react";
-import { MeshPhongMaterial } from "three";
+import React, { Suspense, useEffect, useRef, useState } from "react";
+import { MeshPhongMaterial, SpotLightHelper } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Computers = ({ isMobile }) => {
@@ -9,20 +9,29 @@ const Computers = ({ isMobile }) => {
   // const computer = useGLTF("./testModel.glb");
   const gltf = useLoader(GLTFLoader, "texture/model/untitled.glb");
   console.log(gltf);
+  useEffect(() => {
+    gltf.scene.traverse((node) => {
+      if (node.isMesh) {
+        // console.log(node);
+        node.castShadow = true;
+        // node.receiveShadow = true;
+      }
+    });
+  }, []);
 
   return (
     <mesh receiveShadow>
-      <hemisphereLight intensity={10.15} groundColor="black" />
-      <pointLight intensity={10} />
-      <spotLight
-        position={[0, 50, 10]}
-        angle={0.12}
+      {/* <hemisphereLight intensity={10.15} groundColor="black" /> */}
+      {/* <pointLight intensity={10} /> */}
+      {/* <spotLight
+        position={[0, 20, 10]}
+        angle={3.12}
         penumbra={1}
-        intensity={10}
+        intensity={1}
         color={0xffffff}
         castShadow
         shadow-mapSize={1024}
-      />
+      /> */}
       <primitive
         object={gltf.scene}
         scale={isMobile ? 0.6 : 0.7}
@@ -45,7 +54,25 @@ const Ground = () => {
         shininess={100}
       />
       <planeGeometry args={[20, 20, 20]} receiveShadow />
-      {/* <meshBasicMaterial color={0xffffff} /> */}
+    </mesh>
+  );
+};
+
+const Light = () => {
+  const spotlightref = useRef();
+  useHelper(spotlightref, SpotLightHelper, "red");
+  return (
+    <mesh castShadow>
+      <spotLight
+        position={[0, 20, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={10}
+        color={0xffffff}
+        castShadow
+        shadow-mapSize={1024}
+        ref={spotlightref}
+      />
     </mesh>
   );
 };
@@ -69,22 +96,25 @@ const Trident = () => {
   }, []);
 
   return (
-    <Canvas
-      frameloop="demand"
-      shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-      // 특정한 최적화 수행을 막지만 WebGL이 캔버스를 지우지 않도록 하는 작업
-    >
-      <OrbitControls
-        // enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-        rotateSpeed={0.5}
-      />
-      <Computers isMobile={isMobile} />
-      <Ground />
-    </Canvas>
+    <>
+      <Canvas
+        frameloop="demand"
+        shadows
+        camera={{ position: [20, 3, 5], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}
+        // 특정한 최적화 수행을 막지만 WebGL이 캔버스를 지우지 않도록 하는 작업
+      >
+        <OrbitControls
+          // enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          rotateSpeed={0.5}
+        />
+        <Computers isMobile={isMobile} />
+        <Ground />
+        <Light />
+      </Canvas>
+    </>
   );
 };
 
