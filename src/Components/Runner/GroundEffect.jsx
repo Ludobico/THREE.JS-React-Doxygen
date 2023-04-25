1b574529f78fa1e384a6b4f1888f59a0c4610e7b
Ludobico <aqs450@gmail.com>
2023-04-25 17:20:16 +0900

    point material setting

--- src/Components/Runner/GroundEffect.jsx
+++ src/Components/Runner/GroundEffect.jsx
@@ -1,43 +1,27 @@
+import { Box, Line } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import React, { useRef } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import * as THREE from "three";
 
 const GroundEffect = () => {
-  const itemsRef = useRef([]);
+  const rain_count = 500;
+  const rain_geometry = new THREE.BufferGeometry();
 
-  useFrame((state, delta) => {
-    let elapsed = state.clock.getElapsedTime();
+  const rainPositions = new Float32Array(rain_count * 3);
 
-    for (let i = 0; i < itemsRef.current.length; i++) {
-      let mesh = itemsRef.current[i];
-      let z = (i - 7) * 3.5 + ((elapsed * 0.4) % 3.5) * 2;
-      let dist = Math.abs(z);
-      mesh.position.set(0, 0, -z);
-      mesh.scale.set(1 - dist * 0.04, 1 - dist * 0.04, 1 - dist * 0.04);
+  for (let i = 0; i < rain_count; i++) {
+    rainPositions[i * 3] = Math.random() * 400 - 200;
+    rainPositions[i * 3 + 1] = Math.random() * 500 - 250;
+    rainPositions[i * 3 + 2] = Math.random() * 400 - 200;
+  }
 
-      let colorScale = 1;
-      if (dist > 2) {
-        colorScale = 1 - (Math.min(dist, 12) - 2) / 10;
-      }
-      colorScale *= 0.5;
-
-      if (i % 2 == 1) {
-        mesh.material.emissive = new THREE.Color(8, 0.15, 0.7).multiplyScalar(colorScale);
-      } else {
-        mesh.material.emissive = new THREE.Color(0.1, 0.7, 3).multiplyScalar(colorScale);
-      }
-    }
-  });
+  rain_geometry.setAttribute("position", new THREE.BufferAttribute(rainPositions, 3));
 
   return (
-    <>
-      {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((v, i) => (
-        <mesh castShadow receiveShadow position={[0, 0, 0]} key={i} ref={(el) => (itemsRef.current[i] = el)}>
-          <torusGeometry args={[3.35, 0.05, 16, 100]} />
-          <meshStandardMaterial emissive={[4, 0.1, 0.4]} color={[0, 0, 0]} />
-        </mesh>
-      ))}
-    </>
+    <points>
+      <bufferGeometry attach="geometry" {...rain_geometry} />
+      <pointsMaterial attach="material" size={1} sizeAttenuation color="#ffffff" fog={false} />
+    </points>
   );
 };
 
